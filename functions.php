417fc950670b0f8465a3dfe9186d765cff223ae7
Mateusz Baniuk <61153002+mbaniuk@users.noreply.github.com>
2025-06-19 14:56:01 +0200

    Task 2

--- functions.php
+++ functions.php
@@ -1,6 +1,17 @@
 <?php
 
-function fooz_enqueue_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+function fooz_enqueue_assets() {
+    wp_enqueue_style(
+        'parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    wp_enqueue_script(
+        'fooz-scripts',
+        get_stylesheet_directory_uri() . '/assets/js/scripts.js',
+        array('jquery'),
+        null,
+        true
+    );
 }
-add_action('wp_enqueue_scripts', 'fooz_enqueue_styles');
+add_action('wp_enqueue_scripts', 'fooz_enqueue_assets');
